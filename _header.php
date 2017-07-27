d8b222ca894a5c6cbb76df827d6af3082dd65e36
Arjun Koteshwara Gururaj <P7110666@NESSINBLR.COM>
2017-07-27 17:35:46 +0530

    Changed headr city name

--- _header.php
+++ _header.php
@@ -3,7 +3,7 @@
                 <div class="inBox">
                     <!--<h1 id="logo"><a href='http://code.daypilot.org/77465/angularjs-hotel-room-booking'>AngularJS Hotel Room Booking Tutorial</a></h1>
                     <p id="claim"><a href="http://javascript.daypilot.org/">DayPilot for JavaScript</a> - AJAX Calendar/Scheduling Widgets for JavaScript/HTML5/jQuery/AngularJS</p>-->
-					<h1 id="logo"> Clover Bed & Breakfast, Bangalore  - Room Reservation  </h1>
+					<h1 id="logo"> Clover Bed & Breakfast, Bengaluru  - Room Reservation  </h1>
 					<p id="claim"> Ness.com </p>
                     <hr class="hidden" />
                 </div>
