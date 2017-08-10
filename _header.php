c0ce4ed5c035e35b5e06ae899c6e90448167bd43
Arjun Koteshwara Gururaj <P7110666@NESSINBLR.COM>
2017-08-10 12:54:44 +0530

    Made the ness.com text to appear as a hyperlink & open in a new tab

--- _header.php
+++ _header.php
@@ -4,7 +4,7 @@
                     <!--<h1 id="logo"><a href='http://code.daypilot.org/77465/angularjs-hotel-room-booking'>AngularJS Hotel Room Booking Tutorial</a></h1>
                     <p id="claim"><a href="http://javascript.daypilot.org/">DayPilot for JavaScript</a> - AJAX Calendar/Scheduling Widgets for JavaScript/HTML5/jQuery/AngularJS</p>-->
 					<h1 id="logo"> Clover Bed & Breakfast, Bangalore  - Room Reservation Service </h1>
-					<p id="claim"> Ness.com </p>
+					<p id="claim"> <a target = '_blank' href="https://www.ness.com/">Ness.com</a> </p>
                     <hr class="hidden" />
                 </div>
             </div>
